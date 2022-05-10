915c98ee4ba6b468ea83dedf4b739ae96a9ce758
ouiouid <98252212+ouiouid@users.noreply.github.com>
2022-05-10 16:38:56 +0800

    Size and Animations
    - added buttons for changing number of bars
    - added animated sorting for better visualization

--- src/SortingVisualizer/SortingAlgorithms.js
+++ src/SortingVisualizer/SortingAlgorithms.js
@@ -3,12 +3,12 @@
 export function bubbleSort(array) {
     let changes = [];
     for (let i=0;i<array.length;i++) {
-        changes.push(['p']);
         for (let j=0;j<array.length-1;j++) {
             if (array[j] > array[j+1]) {
                 changes.push([j,j+1]);
                 [array[j], array[j+1]] = [array[j+1], array[j]];
             }
+            
         }
     }
     return changes;
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -3,144 +3,126 @@ import './SortingVisualizer.css';
 import { bubbleSort } from './SortingAlgorithms';
 
 // class component - used state 
-class SortingVisualizer extends React.Component {
+export default class SortingVisualizer extends React.Component {
+    
+    
+    BAR_COLOR = '#FFF';
+    SWAP_COLOR = '#BBB';
 
+    //class properties
     constructor(props) {
         super(props);
 
-        //initialise an empty array
         this.state = {
+            size: 16,
             array: [],
-            size: 128,
+            color: [],
+            changes: [],
         }
     }
 
+    //setup the class
     componentDidMount() {
-        this.createArray();
-        setTimeout(() => { this.applyRandom(); }, 200);
+        this.resetArray_flat();
+        setTimeout(() => { this.resetArray_random();}, 1);
+        setTimeout(() => { this.resetColor();}, 1);
+        setTimeout(() => { this.createChanges();}, 1);
 
-        
     }
 
     //-------------------------------------------------------------------------------------
-
-    // create new array
-    createArray() {
-        const array = [];
-        const numBars = this.state.size; //size
-        for(let i=0; i<numBars;i++) {
-            array.push(0);
-        }
+    
+    //reset the color of the bars
+    resetColor() {
+        let color = new Array(this.state.size).fill(this.BAR_COLOR);
+        this.setState({color:color})
+    }
+    //reset height of bars to flat
+    resetArray_flat() {
+        let array = new Array(this.state.size).fill(0);
         this.setState({array:array})
     }
-
-    //-------------------------------------------------------------------------------------
-
-    // value -> single bar
-    setBar(value,index) {
-        let array = [...this.state.array];
-        array[index] = value;
-        this.setState({array:array})
-    }
-
-
-
-    // value -> entire array
-    setArray_flat(value) {
-        let array = [...this.state.array];
+    //reset height of bars to random
+    resetArray_random() {
+        let array = new Array(this.state.size);
         for(let i=0;i<array.length;i++) {
-            array[i]=value;
+            array[i]=Math.floor(Math.random() * (1000-10) +10)/10;;
         }
         this.setState({array:array});
     }
-
-    // value -> entire array 
-    setArray_flat_wave(value) {
-        for(let i=0;i<this.state.size;i++)
-        {
-            setTimeout(() => { this.setBar(value,i);}, i*2);
-        }
-    }
-
-    //-------------------------------------------------------------------------------------
-
-    applyRandom() {
-        let array = [...this.state.array];
-        for(let i=0;i<array.length;i++) {
-            array[i]=Math.floor(Math.random() * (1000-10+1) +10);;
-        }
-        this.setState({array:array})
-    }
-
-    applyMin() {
-        this.setArray_flat(10)
-    }
-
-    applyMax() {
-        this.setArray_flat(1000)
-    }
-
-    applyBestCase() {
-        let array = [...this.state.array];
+    //reset height of bars to worse case
+    resetArray_worst() {
+        let array = new Array(this.state.size);
         let size = this.state.size;
         for(let i=0;i<array.length;i++) {
-            array[i] = (i*(1000/size))+(1000/size);
+            array[i] = (100-i*(100/size));
         } 
-        this.setState({array});
+        this.setState({array:array});
     }
-
-    applyWorstCase() {
-        let array= [...this.state.array];
-        let size = this.state.size;
-        for(let i=0;i<array.length;i++) {
-            array[i] = 1000-i*(1000/size);
-        } 
-        this.setState({array});
-    }
-
     //-------------------------------------------------------------------------------------
-
-    apply_bubbleSort() {
-        let old_array = [...this.state.array];
-        let changes = bubbleSort([...old_array]);
-        let num_passes = 0;
-        let num_swaps = 0;
-
-
-        for(let i=0;i<changes.length;i++) {
-            if (changes[i] == 'p') {
-                num_passes++;
-            } else {
-                num_swaps++;
-                [old_array[changes[i][0]], old_array[changes[i][1]]]  =  [old_array[changes[i][1]], old_array[changes[i][0]]];
-
-            }
-        }
-
-        this.setState({array: old_array})
-    }
-
-
-    /*function animate_bubble(old_array) {
-        let [bubble_array, changes] = bubbleSort([...old_array]);
-        let num_passes = 0;
-        let num_changes = 0;
-        
-        console.log(changes);
-        for (let i=0;i<changes.length;i++) {
-            if (changes[i] == 'p') {
-                num_passes++;
-                console.log(num_passes);
-            } else {
-                num_changes++;
-                [old_array[changes[i][0]], old_array[changes[i][1]]] = [old_array[changes[i][1]], old_array[changes[i][0]] ];
-                console.log(num_changes+': '+old_array);
-            }   
     
+    //create a new series of steps to complete
+    createChanges() {
+        let changes = bubbleSort([...this.state.array]);
+        let length = changes.length
+        this.setState({changes:changes,numChanges:length})
+    }
+    //sort the current bars using bubble sort
+    apply_bubbleSort() {
+        let speed = (196/this.state.size)*5
+        let numChanges = this.state.numChanges
+        let i=0
+        while (i<numChanges) {
+            i++
+            setTimeout(() => {this.apply_bubbleSort_step();}, i*speed);
+            setTimeout(() => {this.resetColor();}, (i*speed)+(speed));
         }
-    }*/
+        this.setState({numChanges:0})
+    }
+    //a single step in the bubble sort
+    apply_bubbleSort_step() {
+        let new_array = [...this.state.array];
+        let new_color = [...this.state.color];
+        let changes   = [...this.state.changes];
 
+        [new_array[changes[0][0]], new_array[changes[0][1]]]  =  [new_array[changes[0][1]], new_array[changes[0][0]]];
+        new_color[[changes[0][0]]] = this.SWAP_COLOR;
+        new_color[[changes[0][1]]] = this.SWAP_COLOR;
+        changes.shift();
+        this.setState({array: new_array, color: new_color, changes:changes});
+    }
     //-------------------------------------------------------------------------------------
+    
+    button_random() {
+        setTimeout(() => { this.resetArray_random();}, 100);
+        setTimeout(() => { this.resetColor();}, 100);
+        setTimeout(() => { this.createChanges();}, 100);
+
+    }
+
+    button_worst() {
+        setTimeout(() => { this.resetArray_worst();}, 100);
+        setTimeout(() => { this.resetColor();}, 100);
+        setTimeout(() => { this.createChanges();}, 100);
+    }
+
+    button_increase() {
+        if(this.state.size<96){
+            this.button_random();
+            this.setState({size:this.state.size+8});
+            this.button_random();
+        }
+    }
+    
+    button_decrease() {
+        if(this.state.size>8) {
+            this.button_random();
+            this.setState({size:this.state.size-8});
+            this.button_random()
+        }
+    }
+    //-------------------------------------------------------------------------------------
+    
     render() {
         const array = this.state.array;
         return (
@@ -148,9 +130,14 @@ class SortingVisualizer extends React.Component {
 
                 <div className='menu'>
                     <div className='button-list'>
-                        <button className='btn' onClick={()=>this.applyRandom()}>Random</button>
-                        <button className='btn' onClick={()=>this.applyWorstCase()}>Worst</button>
-                        <button  onClick={()=>this.apply_bubbleSort()}>Bubble Sort</button>
+                        <button className='smallbtn' onClick={()=>this.button_decrease()}>-</button>
+                        <button className='smallbtn' onClick={()=>this.button_increase()}>+</button>
+                        <button className='btn' onClick={()=>this.button_random()}>Random</button>
+                        <button className='btn' onClick={()=>this.button_worst()}>Worst</button>
+                        <button onClick={()=>this.apply_bubbleSort()}>Sort</button>
+
+                        
+
                     </div>
                 </div>
 
@@ -159,7 +146,7 @@ class SortingVisualizer extends React.Component {
                         <div 
                             className='bar'
                             key={idx}
-                            style={{height:`${((value)/10.0)}%`}}></div>
+                            style={{background:`${(this.state.color[idx])}`,height:`${((value))}%`}}></div>
                     ))}
                 </div> 
             </div> 
@@ -168,10 +155,3 @@ class SortingVisualizer extends React.Component {
 
 //-----------------------------------------------------------------------------------------
 }
-
-
-function randomIntFromInterval(min, max) {
-    return 
-}
-
-export default SortingVisualizer;
